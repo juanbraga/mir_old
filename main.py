981de625812b8b13e3679b260ee329925b43d27e
jbraga@dell <juanbragabrum@gmail.com>
2016-07-06 00:40:32 -0300

    now loading dataset with traditional_dataset/tradataset.py

--- main.py
+++ main.py
@@ -9,63 +9,20 @@ import numpy as np
 import scipy.signal as signal
 import scipy.io.wavfile as wav
 import csv
-
-def load_trdataset_list():
-
-    ltrdataset=[]    
-    cr = csv.reader(open('../traditional_dataset/dataset.csv',"rb"))
-    for row in cr:
-        ltrdataset.append(row[0]) 
-        
-    return ltrdataset
-    
-def load_gt(gt_file, t):
-    
-    cr = csv.reader(open(gt_file,"rb"))
-    onset=[]
-    notes=[]
-    for row in cr:
-        onset.append(row[0]) 
-        notes.append(row[1])
-    onset = np.array(onset, 'float32')
-    
-    aux_vad_gt = np.empty([0,], 'int8')
-    for note in notes:
-        if note=='0':
-            aux_vad_gt = np.r_[aux_vad_gt,0]
-        else:
-            aux_vad_gt = np.r_[aux_vad_gt,1]
-    
-    j=0
-    vad_gt = np.empty([len(t),], 'int8')
-    for i in range(1,len(onset)):
-        while (j<len(t) and t[j]>=onset[i-1] and t[j]<=onset[i]):
-            vad_gt[j]=aux_vad_gt[i-1]
-            j=j+1  
-    
-    return vad_gt
-    
-def load_audio(audio_file):
-    
-    fs, audio = wav.read(audio_file)
-#    audio = audio.astype('float64')
-    t = np.arange(len(audio)) * float(1)/fs
-    
-    return audio, t, fs    
+import tradataset as td
 
 
 if __name__ == "__main__":  
 
-
-    ltrdataset = load_trdataset_list()    
+    ltrdataset = td.load_list()    
 
     fragment = ltrdataset[8]    
     
     audio_file = fragment + '_mono.wav'
     gt_file = fragment + '.csv'
 
-    audio, t, fs = load_audio(audio_file)
-    vad_gt = load_gt(gt_file, t)    
+    audio, t, fs = td.load_audio(audio_file)
+    vad_gt = td.load_gt(gt_file, t)    
 
 
 #%% SPEC & WAVE WITH GT
@@ -82,11 +39,10 @@ if __name__ == "__main__":
     plt.pcolormesh(t_S, f, 20*np.log(Sxx))
     plt.axis('tight')
     plt.subplot(3,1,3)
-    plt.plot(t, audio, color='black')
+    plt.plot(t, audio, color='black', alpha=0.7)
     plt.grid()
     plt.axis('tight')
-    plt.plot(t, vad_gt*max(audio)/2, color='blue', lw=2)
-    plt.fill_between(t, 0, vad_gt*max(audio)/2, facecolor='blue', alpha=0.2)
+    plt.fill_between(t, -vad_gt*(2**12), vad_gt*(2**12), facecolor='blue', alpha=0.2)
     plt.show()
 
 #%% ESTIMATE VAD
