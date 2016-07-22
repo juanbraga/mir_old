83ff698c2316cae90c9f024596edec863a08c602
jbraga@ThinkPad <juanbragabrum@gmail.com>
2016-07-22 20:29:02 -0300

    score following (?)

--- music_notation_extraction.py
+++ music_notation_extraction.py
@@ -25,34 +25,17 @@ if __name__ == "__main__":
 
     ltrdataset = td.load_list()    
 
-    fragment = ltrdataset[1]    
+    fragment = ltrdataset[9]    
     
     audio_file = fragment + '_mono.wav'
     gt_file = fragment + '.csv'
+    score_file = fragment + '.xml'
 
     audio, t, fs = td.load_audio(audio_file)
     vad_gt, gt, onset_gt = td.load_gt(gt_file, t)    
-    np.place(gt,gt==-np.inf,0)
 
-#%% SPEC & WAVE WITH GT
-    
     frame_size = 1024
-    op = 0
-    
-    f, t_S, Sxx = signal.spectrogram(audio, fs, window='hamming', nperseg=frame_size, 
-                                     noverlap=op, nfft=None, detrend='constant',
-                                     return_onesided=True, scaling='spectrum', axis=-1)
-            
-#    plt.figure(figsize=(18,6))
-#    plt.subplot(3,1,(1,2))
-#    plt.pcolormesh(t_S, f, 20*np.log(Sxx))
-#    plt.axis('tight')
-#    plt.subplot(3,1,3)
-#    plt.plot(t, audio, color='black', alpha=0.6)
-#    plt.grid()
-#    plt.axis('tight')
-#    plt.fill_between(t, -vad_gt*(2**12), vad_gt*(2**12), facecolor='yellow', alpha=0.3)
-#    plt.show()
+    op = 0    
 
 #%% ESTIMATE VAD
 
@@ -73,37 +56,37 @@ if __name__ == "__main__":
     hop = (frame_size-op)
     pitch_midi, timestamps = pe.pitch_extraction(audio, fs, frame_size, hop)
 
-    fig = plt.figure(figsize=(18,6))                                                               
-    ax = fig.add_subplot(3,1,(1,2))                                                      
-    
-    yticks_major = [ 59, 60, 62, 64, 65, 67, 69, 71, 72, 74, 76, 77, 79, 81, 83, 84, 86, 88, 89, 91, 93, 95, 96 ]
-    yticks_minor = [ 61, 63, 66, 68, 70, 73, 75, 78, 80, 82, 85, 87, 90, 92, 94 ]
-#    yticks_labels = ['B3', 'C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5', 'D5', 'E5', 'F5', 'G5', 'A5', 'B5', 'C6', 'D6', 'E6', 'F6', 'G6', 'A6', 'B6', 'C7']                         
-                                             
-    ax.set_yticks(yticks_major)                                                       
-#    ax.set_ytickslabels(yticks_labels)       
-    ax.set_yticks(yticks_minor, minor=True)
-                                    
-    plt.ylim(58, 96) #flute register in midi   
-    plt.xlim(0, t[-1])
-    ax.grid(b=True, which='major', color='black', axis='y', linestyle='-', alpha=0.3)
-    ax.grid(b=True, which='minor', color='black', axis='y', linestyle='-', alpha=1)    
-    
-    plt.plot(timestamps , pitch_midi,'.-',color='red', lw=0.3)
-    plt.fill_between(timestamps, gt-0.5, gt+0.5, facecolor='yellow', label='notes', alpha=0.6)
-
-    plt.title("Melody")
-    plt.ylabel("Pitch (Midi)")
-    
-    plt.subplot(3,1,3)
-    plt.plot(t, audio, color='black', alpha=0.5)
-    plt.grid()
-    plt.axis('tight')
-    plt.fill_between(t, -vad_gt*(2**12), vad_gt*(2**12), facecolor='cyan', alpha=0.6)    
-    plt.title("WaveForm + Activity Detection")
-    plt.ylabel("Amplitude")
-    plt.xlabel("Time (s)")    
-    plt.show()
+#    fig = plt.figure(figsize=(18,6))                                                               
+#    ax = fig.add_subplot(3,1,(1,2))                                                      
+#    
+#    yticks_major = [ 59, 60, 62, 64, 65, 67, 69, 71, 72, 74, 76, 77, 79, 81, 83, 84, 86, 88, 89, 91, 93, 95, 96 ]
+#    yticks_minor = [ 61, 63, 66, 68, 70, 73, 75, 78, 80, 82, 85, 87, 90, 92, 94 ]
+##    yticks_labels = ['B3', 'C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5', 'D5', 'E5', 'F5', 'G5', 'A5', 'B5', 'C6', 'D6', 'E6', 'F6', 'G6', 'A6', 'B6', 'C7']                         
+#                                             
+#    ax.set_yticks(yticks_major)                                                       
+##    ax.set_ytickslabels(yticks_labels)       
+#    ax.set_yticks(yticks_minor, minor=True)
+#                                    
+#    plt.ylim(58, 96) #flute register in midi   
+#    plt.xlim(0, t[-1])
+#    ax.grid(b=True, which='major', color='black', axis='y', linestyle='-', alpha=0.3)
+#    ax.grid(b=True, which='minor', color='black', axis='y', linestyle='-', alpha=1)    
+#    
+#    plt.plot(timestamps , pitch_midi,'.-',color='red', lw=0.3)
+#    plt.fill_between(timestamps, gt-0.5, gt+0.5, facecolor='yellow', label='notes', alpha=0.6)
+#
+#    plt.title("Melody")
+#    plt.ylabel("Pitch (Midi)")
+#    
+#    plt.subplot(3,1,3)
+#    plt.plot(t, audio, color='black', alpha=0.5)
+#    plt.grid()
+#    plt.axis('tight')
+#    plt.fill_between(t, -vad_gt*(2**12), vad_gt*(2**12), facecolor='cyan', alpha=0.6)    
+#    plt.title("WaveForm + Activity Detection")
+#    plt.ylabel("Amplitude")
+#    plt.xlabel("Time (s)")    
+#    plt.show()
     
     
 #%% ONSET DETECTION FROM PITCH CONTOUR
@@ -179,10 +162,27 @@ if __name__ == "__main__":
 
 #%%        
 
+#    from dtw import dtw
+#    x=filtrated_pitch.astype('float64')
+#    x=x.reshape(-1, 1)
+#    y=gt.reshape(-1, 1)
+#    dist, cost, acc, path = dtw(x, y, dist=lambda x, y: np.linalg.norm(x - y, ord=1))
+#    print 'Minimum distance found:', dist
+#    plt.figure()    
+#    plt.imshow(acc.T, origin='lower', cmap='gray', interpolation='nearest')
+#    plt.pcolormesh(acc.T)
+#    plt.plot(path[0], path[1], 'w')
+#    plt.xlim((-0.5, acc.shape[0]-0.5))
+#    plt.ylim((-0.5, acc.shape[1]-0.5))
+    
+    #%%        
+
+    score = td.load_score(score_file)
+
     from dtw import dtw
     x=filtrated_pitch.astype('float64')
     x=x.reshape(-1, 1)
-    y=gt.reshape(-1, 1)
+    y=score.reshape(-1, 1)
     dist, cost, acc, path = dtw(x, y, dist=lambda x, y: np.linalg.norm(x - y, ord=1))
     print 'Minimum distance found:', dist
     plt.figure()    
